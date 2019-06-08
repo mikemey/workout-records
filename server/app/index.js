f08751115d76b0e1058265afebe1bc6653ea1846
mikemey <miguel.maestro@gmx.co.uk>
2019-06-08 22:27:25 +0100

    setting Cache-Control header for static file responses

--- server/app/index.js
+++ server/app/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const { createRequestLogger } = require('./requestsLogger')
 
+const staticfilesOptions = { maxAge: 31536000 }
+
 const createServer = (config, logger) => new Promise((resolve, reject) => {
   const app = express()
   app.use(createRequestLogger(config))
-  app.use('/workout-records', express.static('static/'))
+  app.use('/workout-records', express.static('static/', staticfilesOptions))
 
   const server = app.listen(config.port, config.interface, () => {
     logger.info(`Started on port ${server.address().port}`)
