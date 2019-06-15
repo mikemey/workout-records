961c411892380e94f93a0dc21dc5eb6be5ba65bf
mikemey <miguel.maestro@gmx.co.uk>
2019-06-15 11:07:49 +0100

    add static-assets deploy script

---
+++ server/deploy-static-assets.sh
@@ -0,0 +1,13 @@
+if [[ ! ${WR_SERVER_STATIC_DIR} ]]; then
+    echo "Environment variable '\$WR_SERVER_STATIC_DIR' not set!"
+    exit
+  fi
+
+echo "cleaning static-asset directory $WR_SERVER_STATIC_DIR"
+rm -rf $WR_SERVER_STATIC_DIR/*
+
+echo "copying static assets..."
+cp -r static/*.css $WR_SERVER_STATIC_DIR
+cp -r static/img $WR_SERVER_STATIC_DIR
+cp -r static/template $WR_SERVER_STATIC_DIR
+cp -r static/vendor $WR_SERVER_STATIC_DIR
