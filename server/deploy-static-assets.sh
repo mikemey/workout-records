4ffdd3c8451b5627a928faa8853dc83516468cd7
mikemey <miguel.maestro@gmx.co.uk>
2019-06-16 16:54:35 +0100

    generate CacheFile configuration line when deploying to apache2

--- server/deploy-static-assets.sh
+++ server/deploy-static-assets.sh
@@ -8,7 +8,15 @@ if [[ ! ${WR_SERVER_STATIC_DIR} ]]; then
 echo "cleaning static-asset directory $WR_SERVER_STATIC_DIR"
 rm -rf $WR_SERVER_STATIC_DIR/*
 
+declare OUTPUT=()
 echo "copying static assets..."
-cp -r static/img $WR_SERVER_STATIC_DIR
-cp -r static/template $WR_SERVER_STATIC_DIR
-cp -r static/vendor $WR_SERVER_STATIC_DIR
+OUTPUT+="$(cp -v -r static/img $WR_SERVER_STATIC_DIR)"
+OUTPUT+="$(cp -v -r static/template $WR_SERVER_STATIC_DIR)"
+OUTPUT+="$(cp -v -r static/vendor $WR_SERVER_STATIC_DIR)"
+
+echo -e "\nadd line to apache2 configuration:"
+printf "CacheFile"
+while read -r COPY_OUTPUT; do
+  COPY_TARGET="${COPY_OUTPUT##*-> }"
+  [[ -f "$COPY_TARGET" ]] && printf " $COPY_TARGET"
+done <<< "$OUTPUT"
