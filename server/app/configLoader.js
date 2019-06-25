242a7a018138c055f75f5dd59fab4adb8e643e11
mikemey <miguel.maestro@gmx.co.uk>
2019-06-25 22:25:20 +0100

    add mongo-connection configuration for server

--- server/app/configLoader.js
+++ server/app/configLoader.js
@@ -5,14 +5,22 @@ const defaultEnv = {
   port: 9100,
   interface: '0.0.0.0',
   staticOptions: {},
-  serverPath: '/workout-records'
+  serverPath: '/workout-records',
+  mongodb: {
+    url: 'mongodb://127.0.0.1:27017',
+    dbName: 'workout-local'
+  }
 }
 
 const prodEnv = {
   port: 8002,
   interface: '127.0.0.1',
   requestslog: 'wr-server.requests.log',
-  staticOptions: { maxAge: 86400000 }
+  staticOptions: { maxAge: 86400000 },
+  mongodb: {
+    url: 'mongodb://127.0.0.1:27017',
+    dbName: 'workout-records'
+  }
 }
 
 const message = environment => `using ${environment} environment configuration`
